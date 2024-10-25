1d17a7dceee5e32cf2d1b0f53c70547f15dc4b94
Daniel Santiago Aguila Torres <daniel.santiago.aguila.torres@gmail.com>
2024-10-24 22:04:41 -0700

    fix: corretly export list and function files

--- src/compose-it.scm
+++ src/compose-it.scm
@@ -1,6 +1,8 @@
 (library (compose-it)
   (export make-left left? left-value make-right right? right-value
-	  either-flatmap either-map)
+	  either-flatmap either-map compose curry flip foldr foldl flatmap
+	  intersperse cartesian-product reject zip times take take-last drop
+	  drop-last range)
   (import (compose-it either)
 	  (compose-it function)
 	  (compose-it list)))
